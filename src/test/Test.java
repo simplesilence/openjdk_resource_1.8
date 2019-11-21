a118678817ff67fea442ca0f0d7a2d11d06713c3
simplesilence <1317495534@qq.com>
2019-11-21 17:55:56 +0800

    Initial commit

---
+++ src/test/Test.java
@@ -0,0 +1,13 @@
+package test;
+
+import java.util.HashMap;
+
+public class Test {
+
+
+
+    public static void main(String[] args) {
+        HashMap map = new HashMap();
+        System.out.println(map);
+    }
+}
